cc48f975f35cfe8020fa7bfb5717a453dfb4d293
Jair Leonardo Jimenez Munevar <jairmunevar@gmail.com>
2024-04-10 21:58:15 -0500

    Initial commit

---
+++ webpack.prod.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '',
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg)$/,
+        type: 'asset',
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env'],
+            plugins: ['@babel/plugin-transform-class-properties'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles.[contenthash].css',
+    }),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Hello World',
+      description: 'Some description for meta',
+      template: 'src/template.html',
+    }),
+  ],
+};
+
+/**
+ * TYPE RESOURCES
+ * - asset/resource: generates the file and add an URL to the resource
+ * - asset/inline: generate a base64 enconded in the place where the resource is used
+ * - asset: webpack will decide it automatically; less than 8Kb the resource is going to be treated
+ *          as asset/inline, otherwise it will be treated as asset/resource.
+ *          It accepts an option to determine the size of the file
+ *          parser: {
+ *            dataUrlCondition: { maxSize: 3 * 1024; // 3 kilobytes }
+ *          }
+ */
+
+/**
+ * LOADERS
+ * - There are some loaders that came with webpack, other ones need to be installed as a devDependency
+ */
