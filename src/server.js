603689965733454c6cbb28fe54ed03bbb5e3a075
Jair Leonardo Jimenez Munevar <jairmunevar@gmail.com>
2024-04-26 16:36:50 -0500

    serve static files using express

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const fs = require('fs');
+const fs = require("fs");
+
+app.use("/static", express.static(path.resolve(__dirname, "../dist")));
 
 app.get("/", function (req, res) {
   const pathToHtmlFile = path.resolve(__dirname, "../dist/index.html");
